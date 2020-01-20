1b8c09ca87d9ebfc66fb167578a0854c2b8ab484
Julio Henrique <juliohenrique@Julios-MacBook-Air.local>
2020-01-19 22:02:02 -0300

    implement web socket

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
 const routes = require('./routes');
+
 const app = express();
+const server = http.Server(app);
 
 mongoose.connect('mongodb+srv://userdbmongo:userdbmongo@cluster0-digsm.mongodb.net/week10?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3000)
+server.listen(3333)
