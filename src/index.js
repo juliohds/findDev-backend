0cb8ca839907cb1cb55c66777b0b890a57d7eac2
Julio Henrique <juliohenrique@Julios-MacBook-Air.local>
2020-01-19 23:17:17 -0300

    master backend

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const http = require('http');
 const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
 
 const app = express();
 const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect('mongodb+srv://userdbmongo:userdbmongo@cluster0-digsm.mongodb.net/week10?retryWrites=true&w=majority', {
   useNewUrlParser: true,
